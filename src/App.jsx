f8152230d17b01f4561cda70ab397d04ebbb2aac
Vishesh Kumar <vishesh.20b0121209@abes.ac.in>
2024-03-28 10:36:13 +0530

    design Component changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,129 +1,179 @@
-import React from 'react';
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
-
+import React from "react";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  Stack,
+} from "@mui/material";
+import { styled } from "@mui/material/styles";
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+}));
 function NestedTable() {
   return (
-    <TableContainer component={Paper} sx={{border:"1px solid black"}}>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell style={{border:"1px solid black"}}>Patient Service Phase</TableCell>
-            <TableCell sx={{textAlign:"center"}} style={{border:"1px solid black"}}>Lab Investigations</TableCell>
-            <TableCell sx={{textAlign:"center"}} style={{border:"1px solid black"}}>Imageology</TableCell>
-            <TableCell sx={{textAlign:"center"}} style={{border:"1px solid black"}}>Pharmacy</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          <TableRow>
-            <TableCell>
-            <TableContainer component={Paper} >
-                <Table>
-                  <TableHead>
-                    <TableRow>
-                      <TableCell style={{border:"1px solid black"}}></TableCell>
-                    </TableRow>
-                    <TableRow>
-                      <TableCell style={{border:"1px solid black"}}>Pre-OP</TableCell>
-                    </TableRow>
-                    <TableRow>
-                      <TableCell style={{border:"1px solid black"}}>OT</TableCell>
-                    </TableRow>
-                    <TableRow>
-                      <TableCell style={{border:"1px solid black"}}>Post-OP</TableCell>
-                    </TableRow>
-                    <TableRow>
-                      <TableCell style={{border:"1px solid black"}}>Post Treatment Evidence</TableCell>
-                    </TableRow>
-                  </TableHead>
-                </Table>
-              </TableContainer>
-            </TableCell>
-            <TableCell >
-              <TableContainer component={Paper} sx={{border:"1px solid black"}}>
-                <Table>
-                  <TableHead>
-                    <TableRow>
-                      <TableCell style={{border:"1px solid black"}}>SNO</TableCell>
-                      <TableCell style={{border:"1px solid black"}}>Name</TableCell>
-                      <TableCell style={{border:"1px solid black"}}>Unit Price</TableCell>
-                    </TableRow>
-                  </TableHead>
-                  <TableBody>
-                    <TableRow>
-                      <TableCell>Nested Table Cell 2</TableCell>
-                    </TableRow>
-                    {/* Add more rows as needed */}
-                  </TableBody>
-                </Table>
-              </TableContainer>
-            </TableCell>
-            <TableCell>
-              <TableContainer component={Paper} sx={{border:"1px solid black"}}> 
-                <Table>
-                  <TableHead>
-                    <TableRow>
-                      <TableCell style={{border:"1px solid black"}}>SNO</TableCell>
-                      <TableCell style={{border:"1px solid black"}}>Name</TableCell>
-                      <TableCell style={{border:"1px solid black"}}>Unit Price</TableCell>
-                    </TableRow>
-                  </TableHead>
-                  <TableBody>
-                    <TableRow>
-                      <TableCell>Nested Table Cell 2</TableCell>
-                    </TableRow>
-                    {/* Add more rows as needed */}
-                  </TableBody>
-                </Table>
-              </TableContainer>
-            </TableCell>
-            <TableCell >
-            <TableContainer component={Paper} sx={{border:"1px solid black"}}>
-                <Table >
-                  <TableHead>
-                    <TableRow>
-                      <TableCell  style={{border:"1px solid black"}} sx={{textAlign:"center"}}>Drugs</TableCell>
-                      <TableCell style={{border:"1px solid black"}} sx={{textAlign:"center"}}>Consumables/Implants</TableCell>
-                    </TableRow>
-                  </TableHead>
-                  <TableBody >
-                    <TableRow >
-                      <TableCell >
-                        <Table sx={{border:"1px solid black"}}>
-                          <TableHead>
-                            <TableRow >
-                              <TableCell style={{border:"1px solid black"}}>SNO</TableCell>
-                              <TableCell style={{border:"1px solid black"}}>Name</TableCell>
-                              <TableCell style={{border:"1px solid black"}}>Quantity</TableCell>
-                              <TableCell style={{border:"1px solid black"}} >Unit Price</TableCell>
-                            </TableRow>
-                          </TableHead>
-                        </Table>
-                      </TableCell>
-                      <TableCell>
-                        <Table sx={{border:"1px solid black"}}>
-                          <TableHead>
-                            <TableRow>
-                              <TableCell style={{border:"1px solid black"}}>SNO</TableCell>
-                              <TableCell style={{border:"1px solid black"}}>Name</TableCell>
-                              <TableCell style={{border:"1px solid black"}}>Route</TableCell>
-                              <TableCell style={{border:"1px solid black"}}>Strength</TableCell>
-                              <TableCell style={{border:"1px solid black"}}>Dosage</TableCell>
-                              <TableCell style={{border:"1px solid black"}}>Days</TableCell>
-                              <TableCell style={{border:"1px solid black"}}>Unit Price</TableCell>
-                            </TableRow>
-                          </TableHead>
-                        </Table>
-                      </TableCell>
-                    </TableRow>
-                    {/* Add more rows as needed */}
-                  </TableBody>
-                </Table>
-              </TableContainer>
+    <>
+      <TableContainer component={Paper} sx={{ border: "1px solid black" }}>
+        <Table sx={{ width: "100%" }}>
+          <TableHead sx={{ m: "auto" }}>
+            <TableRow>
+              <TableCell 
+                sx={{ textAlign: "center" }}
+                style={{ border: "1px solid black" }}>
+                Patient Service Phase
               </TableCell>
-          </TableRow>
-        </TableBody>
-      </Table>
-    </TableContainer>
+              <TableCell
+                sx={{ textAlign: "center" }}
+                style={{ border: "1px solid black" }}
+              >
+                Lab Investigations
+              </TableCell>
+              <TableCell
+                sx={{ textAlign: "center" }}
+                style={{ border: "1px solid black" }}
+              >
+                Imageology
+              </TableCell>
+              <TableCell
+                sx={{ textAlign: "center" }}
+                style={{ border: "1px solid black" }}
+              >
+                Pharmacy
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <TableRow>
+              <Stack sx={{ mt: "20px" }}>
+                <Item sx={{ border: "1px solid black" , borderRadius:"0"}}> </Item>
+                <Item sx={{ border: "1px solid black" , borderRadius:"0"}}> Pre-OP</Item>
+                <Item sx={{ border: "1px solid black" , borderRadius:"0"}}> OT</Item>
+                <Item sx={{ border: "1px solid black" , borderRadius:"0"}}> Post-OP</Item>
+                <Item sx={{ border: "1px solid black" , borderRadius:"0"}}>
+                  Post Treatment Evidence
+                </Item>
+              </Stack>
+
+              <TableCell>
+                <TableContainer
+                  component={Paper}
+                  sx={{ border: "1px solid black" }}
+                >
+                  <Table>
+                    <TableHead>
+                      <TableRow>
+                        <Stack direction="row">
+                          <Item sx={{ border: "1px solid black" , borderRadius:"0"}}> SNO</Item>
+                          <Item sx={{ border: "1px solid black" , borderRadius:"0"}}> Name</Item>
+                          <Item sx={{ border: "1px solid black" , borderRadius:"0"}}> Unit Price</Item>
+                        </Stack>
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                      <TableRow>
+                        <TableCell>Nested Table Cell 2</TableCell>
+                      </TableRow>
+                      {/* Add more rows as needed */}
+                    </TableBody>
+                  </Table>
+                </TableContainer>
+              </TableCell>
+              <TableCell>
+                <TableContainer
+                  component={Paper}
+                  sx={{ border: "1px solid black" }}
+                >
+                  <Table>
+                    <TableHead>
+                      <TableRow>
+                        <Stack direction="row">
+                          <Item sx={{ border: "1px solid black" , borderRadius:"0"}}> SNO</Item>
+                          <Item sx={{ border: "1px solid black" , borderRadius:"0"}}> Name</Item>
+                          <Item sx={{ border: "1px solid black" , borderRadius:"0"}}> Unit Price</Item>
+                        </Stack>
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                      <TableRow>
+                        <TableCell>Nested Table Cell 2</TableCell>
+                      </TableRow>
+                      {/* Add more rows as needed */}
+                    </TableBody>
+                  </Table>
+                </TableContainer>
+              </TableCell>
+              <TableCell>
+                <TableContainer
+                  component={Paper}
+                  sx={{ border: "1px solid black" }}
+                >
+                  <Table>
+                    <TableHead>
+                      <TableRow>
+                        <Stack   direction="row" sx={{display:"flex" ,alignItems:"center" ,justifyContent:"center"}}> 
+                          <Item sx={{borderRight:"1px solid black" , borderRadius:"0"}}>Drugs</Item>
+                          <Item sx={{borderRight:"1px solid black" , borderRadius:"0"}}>Consumables/Implants</Item>
+                        </Stack>
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                      <TableRow>
+                        <TableCell>
+                        <Table sx={{border:"1px solid black"}}>
+                    <TableHead>
+                      <TableRow>
+                        <Stack direction="row" >
+                          <Item sx={{ borderBottom: "1px solid black",borderRight: "1px solid black" , borderRadius:"0"}}> SNO</Item>
+                          <Item sx={{ borderBottom: "1px solid black",borderRight: "1px solid black" , borderRadius:"0"}}> Name</Item>
+                          <Item sx={{ borderBottom: "1px solid black",borderRight: "1px solid black" , borderRadius:"0"}}> UnitPrice</Item>
+                        </Stack>
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                      <TableRow>
+                        <TableCell>Nested Table Cell 2</TableCell>
+                      </TableRow>
+                      {/* Add more rows as needed */}
+                    </TableBody>
+                  </Table>
+                        </TableCell>
+                        <TableCell>
+                        <Table sx={{border:"1px solid black"}}>
+                    <TableHead>
+                      <TableRow>
+                        <Stack direction="row" >
+                        <Item sx={{ borderBottom: "1px solid black",borderRight: "1px solid black"}}> SNO</Item>
+                          <Item sx={{ borderBottom: "1px solid black",borderRight: "1px solid black"}}> Name</Item>
+                          <Item sx={{ borderBottom: "1px solid black",borderRight: "1px solid black"}}> UnitPrice</Item>
+                        </Stack>
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                      <TableRow>
+                        <TableCell>Nested Table Cell 2</TableCell>
+                      </TableRow>
+                      {/* Add more rows as needed */}
+                    </TableBody>
+                  </Table>
+                        </TableCell>
+                      </TableRow>
+                      {/* Add more rows as needed */}
+                    </TableBody>
+                  </Table>
+                </TableContainer>
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
   );
 }
 
